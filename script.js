bd67633a1e6754919d57501b6621b32348e4033b
2107BswWux <87951437+2107BswWux@users.noreply.github.com>
2021-10-13 18:24:24 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// console.log("script is linked");
+// loops - loops allow us to loop through data / ccomparrisons
+
+// for loops
+
+for ( let i = 0; 1 < 10; i++){
+    // check the console to see what is happening change the number 10 to a different number, what is happening now????
+
+    // let i = 0; - we first declear a variable and set its data to 0 this is because values always start at 0;
+
+    // we then specify a condition
+
+    // when the condition become flase the loop will stop
+    console.log(i);
+}
+
+// for(let i = 0; i < 20; i++){
+    // console.log(i);
+
+    // we can then add in if statments
+
+    // if (i === 17){
+        // console.log('comment logged at number 17');
+        // continue means skip over logging 17 and jump to next itteration
+        // continue;
+
+        // will stop the for loop
+        // break;
+    // }
+    // console.log(i);
+// }
+
+
+// const fruit = ['apple', 'pear', 'banana', 'mango','dragon fruit', 'blue berry'];
+// console.log(fruit);
+
+// here we are looking at looping through an array we can do this by sepcifing the array name and appending .length to it
+
+
+// for(let i = 0; i < fruit.length; i++ ){
+//     console.log(i);
+//     console.log(fruit[i]);
+// }
+
+
+
+
+// const pokemon = ['charmander', 'mew', 'alakazam', 'aryodactyl'];
+
+// for of loop
+
+// like the for loop we first declear a variable name
+// and then we declear the name of our array
+// the for of loop works exactly the same as a for loop its just a more seamless way of writing it out
+
+// for(let name of pokemon){
+    // console.log(name);
+    // if(name === 'mew'){
+    //     document.write('i choose you mew');
+
+        // will stop the loop if the condiditon is true
+    //     break;
+    // }
+    // console.log(pokemon.indexOf(name));
+// }
+
+// this other method of loop can only be used on arrays
+
+
+
+
+
+
+
+
+
